f8b25bf7698a16c200eb83510c7f15aef99c35de
RaymondKirk <ray.tunstill@live.co.uk>
2017-02-17 15:30:45 +0000

    Updated CMake structure to build programs independantly

---
+++ Sequential/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+
+#include "BlockMatching.hpp"
+
+int main(int argc, char **argv)
+{
+	std::cout << "SEQUENTIAL" << std::endl;
+	return 0;
+}
