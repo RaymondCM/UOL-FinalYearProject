eccdedcc7eef377a891b1559b735d796f4d6809f
RaymondKirk <ray.tunstill@live.co.uk>
2017-02-22 22:56:22 +0000

    Updated kernel and added some small optimisations to main loop in parallel main

--- Parallel/opencl/kernels.cl
+++ Parallel/opencl/kernels.cl
@@ -1,30 +1,30 @@
 #define width 800
 #define height 600 
 
-__kernel void shift(__read_only image2d_t src, uint blockSize, __read_write  image2d_t dst)
+__kernel void motion_estimation(
+	__read_only image2d_t prev, 
+	__read_only image2d_t curr, 
+	uint blockSize,
+	__global int2 * motionVectors
+)
 {
-    int x = get_global_id(0) * blockSize;
-    int y = get_global_id(1) * blockSize;
+	const int x = get_global_id(0), y = get_global_id(1);
+	const int2 pos = {x * blockSize, y * blockSize };
+	const int blockCount = get_global_size(0);
 
-	if(x < width && y < height) {
+	if (x == 0 && y == 0) {
+		//printf("Block Count: %i\n", blockCount);
+	}
+
+	if(pos.x < width && pos.y < height) {
 		//write_imageui(dst, (int2)(x, y), (uint4)x);
-		printf("(X: %i, Y: %i)\n", x, y);
+		int mvPos = x + y * blockCount;
+		//printf("(X: %i, Y: %i, MV: %i)\n", pos.x, pos.y, mvPos);
+		motionVectors[mvPos] = pos;
 	}
 	else {
-		printf("Outside of bounds (X: %i, Y: %i)\n", x, y);
+		//printf("Outside of bounds (X: %i, Y: %i)\n", pos.x, pos.y);
 	}
 
     //printf("global id = %d, local id = %d\n", x, get_local_id(0)); //do it for each work item
-}
-
-__kernel void add(__global const int *A, __global const int *B, __global int *C)
-{
-    int id = get_global_id(0);
-    if (id == 0)
-    { //perform this part only once i.e. for work item 0
-        printf("work group size %d\n", get_local_size(0));
-    }
-    int loc_id = get_local_id(0);
-    printf("global id = %d, local id = %d\n", id, loc_id); //do it for each work item
-    C[id] = A[id] + B[id];
 }
--- Parallel/src/main.cpp
+++ Parallel/src/main.cpp
@@ -19,6 +19,7 @@
 #include <opencv2/core/ocl.hpp>
 
 #include "CLContext.hpp"
+#include "Timer.hpp"
 
 int main(int argc, char **argv)
 {
@@ -40,17 +41,39 @@ int main(int argc, char **argv)
     CLContext clUtil(argc, argv);
 	cl::Context context = clUtil.GetContext();
 
+	//Load and build kernel file (device code)
+	cl::Program::Sources sources;
+	clUtil.AddSources(sources, kernelFile);
+
+	//Create program with sources for device
+	cl::Program program(context, sources);
+
+	//Attempt to build program
+	try {
+		program.build();
+	}
+	catch (cl::Error err) {
+		std::cerr << err.what() << std::endl;
+		std::cout << "Build Status: " << program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
+		std::cout << "Build Options:\t" << program.getBuildInfo<CL_PROGRAM_BUILD_OPTIONS>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
+		std::cout << "Build Log:\t " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
+		throw err;
+	}
+
+	//Create command queue for context (outside of the loop?)
+	cl::CommandQueue queue(context);
+
 	//Open Video Capture to File
 	cv::VideoCapture VC(dataPath);
 
 	//Allocate Mat for previous and current frame
-	cv::Mat prev, curr;
-	VC >> prev;
+	cv::Mat prev, curr, prevGray, currGray;
 	VC >> curr;
 
 	//Define BM parameters
 	int width = VC.get(cv::CAP_PROP_FRAME_WIDTH), height = VC.get(cv::CAP_PROP_FRAME_HEIGHT);
 	int blockSize = width / 16, wB = width / blockSize, hB = height / blockSize;
+	const int bCount = wB * hB;
 
 	//Tell OpenCV to use OpenCL
 	cv::ocl::setUseOpenCL(true);
@@ -61,10 +84,13 @@ int main(int argc, char **argv)
 	//Should the image file loop?
 	bool loop = true;
 
-	try {
+	//Create Timer to time each frame loop
+	Timer t;
 
+	try {
 		do {
-			VC >> prev; //TODO: DOnt skip frames
+			t.start();
+			curr.copyTo(prev); //TODO: Test skipping frames
 			VC >> curr;
 
 			if (prev.empty() || curr.empty()) {
@@ -79,168 +105,62 @@ int main(int argc, char **argv)
 				break;
 			}
 
-			cvtColor(prev, prev, cv::COLOR_BGR2GRAY);
-			cvtColor(curr, curr, cv::COLOR_BGR2GRAY);
+			cv::cvtColor(prev, prevGray, cv::COLOR_BGR2GRAY);
+			cv::cvtColor(curr, currGray, cv::COLOR_BGR2GRAY);
 
-			//Create command queue for context (outside of the loop?)
-			cl::CommandQueue queue(context);
+			//Convert data (int) to char array
+			char *prevBuffer = reinterpret_cast<char *>(prevGray.data);
+			char *currBuffer = reinterpret_cast<char *>(currGray.data);
 
-			//Load and build kernel file (device code)
-			cl::Program::Sources sources;
-			clUtil.AddSources(sources, kernelFile);
-
-			//Create program with sources for device
-			cl::Program program(context, sources);
-
-			//Attempt to build program
-			try {
-				program.build();
-			}
-			catch (cl::Error err) {
-				std::cerr << err.what() << std::endl;
-				std::cout << "Build Status: " << program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
-				std::cout << "Build Options:\t" << program.getBuildInfo<CL_PROGRAM_BUILD_OPTIONS>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
-				std::cout << "Build Log:\t " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
-				throw err;
-			}
-
-			char *prevBuffer = reinterpret_cast<char *>(prev.data);
-			char *currBuffer = reinterpret_cast<char *>(curr.data);
-
-			cl_mem_flags flags = CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR;
+			//Create image2d_t from buffer (CL_INTENSITY = uint4(I,I,I,I))
+			//CL_UNSIGNED_INT8 for write_imageui
 			cl::ImageFormat fmt(CL_INTENSITY, CL_UNSIGNED_INT8);
+			cl::Image2D prevImage(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, fmt, width, height, 0, prevBuffer);
+			cl::Image2D currImage(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, fmt, width, height, 0, currBuffer);
 
-			cl::Image2D prevImage(context, flags, fmt, width, height, 0, prevBuffer);
-			cl::Image2D currImage(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, fmt, width, height, 0, currBuffer);
+			//Create buffer to store motion vectors for blocks of wB * hB (bCount)
+			cl::Buffer motionVectors(context, CL_MEM_WRITE_ONLY, sizeof(cl_int2) * bCount);
 
-
-			//Create shift kernel
-			cl::Kernel kernel(program, "shift");
+			//Create motion_estimation kernel and set arguments 
+			cl::Kernel kernel(program, "motion_estimation");
 			kernel.setArg(0, prevImage);
-			kernel.setArg(1, (unsigned int) blockSize);
-			kernel.setArg(2, currImage);
+			kernel.setArg(1, currImage);
+			kernel.setArg(2, (unsigned int)blockSize);
+			kernel.setArg(3, motionVectors);
 
-			queue.enqueueNDRangeKernel(kernel, 0, cl::NDRange((size_t)wB, (size_t)hB, 1));
+			cl::NDRange global((size_t)wB, (size_t)hB, 1);
+			queue.enqueueNDRangeKernel(kernel, 0, global);
 
-			// Download the dst data from the device (?)
-			//cv::Mat mat_dst = uOut.getMat(cv::ACCESS_READ);
-			cl::size_t<3> origin;
-			origin[0] = 0;
-			origin[1] = 0;
-			origin[2] = 0;
-			cl::size_t<3> region;
-			region[0] = (size_t)width;
-			region[1] = (size_t)height;
-			region[2] = 1;
+			//Read motion vector buffer from device
+			cl_int2 * mVecBuffer = new cl_int2[bCount];
+			queue.enqueueReadBuffer(motionVectors, 0, 0, sizeof(cl_int2) * bCount, mVecBuffer);
 
-			queue.enqueueReadImage(currImage, 0, origin, region, 0, 0, currBuffer);
+			//Draw Motion Vectors from mVecBuffer
+			for (size_t i = 0; i < wB; i++)
+			{
+				for (size_t j = 0; j < hB; j++)
+				{
+					int id = i + j * wB;
 
-			cv::imshow(dataPath, cv::Mat(height, width, CV_8UC1, &currBuffer[0]));
-		} while ((char)cv::waitKey(100) != 27); //Do while !Esc
+					//Offset drawn point to represent middle rather than top left of block
+					cv::Point offset(blockSize / 2, blockSize / 2);
+					cv::Point pos(i * blockSize, j * blockSize);
+					cv::Point mVec(mVecBuffer[id].x, mVecBuffer[id].y);
+
+					cv::rectangle(curr, pos, pos + (offset * 2), cv::Scalar(255));
+					cv::arrowedLine(curr, pos + offset, mVec + offset, cv::Scalar(255));
+				}
+			}
+
+			t.end();
+
+			cv::imshow(dataPath, curr);
+		} while ((char)cv::waitKey(1) != 27); //Do while !Esc
 	}
 	catch (cl::Error err) {
 		std::cerr << "ERROR: " << err.what() << ", " << clUtil.GetErrorString(err.err()) << std::endl;
 		throw err;
 	}
 
-	return 0; 
-    try
-    {
-        cv::VideoCapture V(dataPath);
-        //cv::Mat prev, curr;
-        cv::Mat prev, curr;
-        V >> prev;
-        V >> curr;
-
-        int width = V.get(cv::CAP_PROP_FRAME_WIDTH), height = V.get(cv::CAP_PROP_FRAME_HEIGHT);
-        cvtColor(prev, prev, cv::COLOR_BGR2GRAY);
-        cvtColor(curr, curr, cv::COLOR_BGR2GRAY);
-
-        cl_int res = CL_SUCCESS;
-        cl_uint num_entries = 0;
-
-        res = clGetPlatformIDs(0, 0, &num_entries);
-        if (CL_SUCCESS != res)
-            return -1;
-
-        std::cout << "Have OpenCL?: " << cv::ocl::haveOpenCL() << std::endl;
-
-        // cv::imshow("PREV", prev);
-        // cv::imshow("CURR", curr);
-        // cv::waitKey(50000);
-
-        if (!cv::ocl::haveOpenCL())
-        {
-            std::cout << "OpenCL is not avaiable..." << std::endl;
-            return -1;
-        }
-
-        cv::ocl::Context context;
-        if (!context.create(cv::ocl::Device::TYPE_GPU))
-        {
-            std::cout << "Failed creating the context..." << std::endl;
-            return -1;
-        }
-
-        // In OpenCV 3.0.0 beta, only a single device is detected.
-        std::cout << context.ndevices() << " GPU devices are detected." << std::endl;
-        for (int i = 0; i < context.ndevices(); i++)
-        {
-            cv::ocl::Device device = context.device(i);
-            std::cout << "name                 : " << device.name() << std::endl;
-            std::cout << "available            : " << device.available() << std::endl;
-            std::cout << "imageSupport         : " << device.imageSupport() << std::endl;
-            std::cout << "OpenCL_C_Version     : " << device.OpenCL_C_Version() << std::endl;
-            std::cout << std::endl;
-        }
-
-        // Select the first device
-        cv::ocl::Device(context.device(0));
-
-        // Transfer Mat data to the device
-        cv::Mat mat_src = prev;
-        mat_src.convertTo(mat_src, CV_32F, 1.0 / 255);
-        cv::UMat umat_src = mat_src.getUMat(cv::ACCESS_READ, cv::USAGE_ALLOCATE_DEVICE_MEMORY);
-        cv::UMat umat_dst(curr.size(), CV_32F, cv::ACCESS_WRITE, cv::USAGE_ALLOCATE_DEVICE_MEMORY);
-
-        std::ifstream ifs(kernelFile);
-        if (ifs.fail())
-            return -1;
-        std::string kernelSource((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
-        cv::ocl::ProgramSource programSource(kernelSource);
-
-        // Compile the kernel code
-        cv::String errmsg;
-        cv::String buildopt = cv::format("-D dstT=%s", cv::ocl::typeToStr(umat_dst.depth())); // "-D dstT=float"
-        cv::ocl::Program program = context.getProg(programSource, buildopt, errmsg);
-
-        cv::ocl::Image2D image(umat_src);
-        float shift_x = -100.5;
-        float shift_y = -50.0;
-        cv::ocl::Kernel kernel("shift", program);
-        kernel.args(image, shift_x, shift_y, cv::ocl::KernelArg::ReadWrite(umat_dst));
-
-        size_t globalThreads[3] = {(size_t)mat_src.cols, (size_t)mat_src.rows, 1};
-        //size_t localThreads[3] = { 16, 16, 1 };
-        bool success = kernel.run(3, globalThreads, NULL, true);
-
-        if (!success)
-        {
-            std::cout << "Failed running the kernel..." << std::endl;
-            return -1;
-        }
-
-        // Download the dst data from the device (?)
-        cv::Mat mat_dst = umat_dst.getMat(cv::ACCESS_READ);
-
-        cv::imshow("src", mat_src);
-        cv::imshow("dst", mat_dst);
-        cv::waitKey();
-    }
-    catch (cl::Error err)
-    {
-        std::cerr << "ERROR: " << err.what() << ", " << clUtil.GetErrorString(err.err()) << std::endl;
-    }
-
-    return 0;
+	return 0;
 }
