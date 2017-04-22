efe8e254cbb6fe5abb5012a21fd030b97093e366
RaymondKirk <ray.tunstill@live.co.uk>
2017-04-22 18:46:50 +0100

    Simplified Scripts and Renamed conversion script

--- scripts/DicomInfo.m
+++ scripts/DicomInfo.m
@@ -1,9 +1,6 @@
 % Select DICOM file
-[in_file_name,in_file_root] = uigetfile('*.dcm','Select the DICOM image file');
-in_data_path = strcat(in_file_root, in_file_name);
+[file_name, root] = uigetfile('*.dcm');
+file_path = strcat(root, file_name);
 
-% Read in DICOM file information
-D = dicominfo(in_data_path);
-
-%% Print Information
-D
+% Read in DICOM file information and print
+D = dicominfo(file_path)
